f62b77a328c5d5c2195b0e7fdfa136fe59150601
Angie G <angieg@Angies-MBP.fios-router.home>
2021-12-16 12:03:53 -0500

    Added Logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,13 @@ class Nav extends React.Component {
     render () {
       return (
       <div className="nav">
-            <Link to='/'></Link>
+            <Link to='/'>
+                <img 
+                className='logo'
+                src='https://i.imgur.com/90XAmJe.png'
+                alt='Graphic Shape Logo'
+                />
+            </Link>
             <Link to='/movies'>Movies</Link>
             <Link to='/people'>People</Link>
             <Link to='/locations'>Locations</Link>
